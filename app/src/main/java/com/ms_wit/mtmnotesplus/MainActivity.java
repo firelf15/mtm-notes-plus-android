ef211e7e5eab4d05bf6cb79ae5934ed06d456c63
Monika Spielman <monika@Monikas-MBP.metaltoad.net>
2015-12-30 10:44:54 -0800

    MTP-59 adding note icons that still need to be re-colored

--- app/src/main/java/com/ms_wit/mtmnotesplus/MainActivity.java
+++ app/src/main/java/com/ms_wit/mtmnotesplus/MainActivity.java
@@ -42,9 +42,9 @@ implements LoaderManager.LoaderCallbacks<Cursor>
         setContentView(R.layout.activity_main);
 
         String[] from = {DBOpenHelper.NOTE_TEXT};
-        int[] to = {android.R.id.text1};
+        int[] to = {R.id.tvNote};
         cursorAdapter = new SimpleCursorAdapter(this,
-                android.R.layout.simple_list_item_1, null, from, to, 0);
+                R.layout.note_list_item, null, from, to, 0);
 
         ListView list = (ListView) findViewById(android.R.id.list);
         list.setAdapter(cursorAdapter);
