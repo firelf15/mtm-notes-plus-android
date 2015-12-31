e452d7d63902e7b5c52dbbf26fafdfb4c7ecf09c
Monika Spielman <monika@Monikas-MBP.metaltoad.net>
2015-12-30 16:41:42 -0800

    MTP-64 whew that was a tough one. minor details in punctuation.

--- app/src/main/java/com/ms_wit/mtmnotesplus/EditorActivity.java
+++ app/src/main/java/com/ms_wit/mtmnotesplus/EditorActivity.java
@@ -2,6 +2,7 @@ package com.ms_wit.mtmnotesplus;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,11 +13,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditorActivity extends AppCompatActivity {
 
     private String action;
     private EditText editor;
+    private String noteFilter;
+    private String oldText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +32,22 @@ public class EditorActivity extends AppCompatActivity {
         editor = (EditText) findViewById(R.id.editText);
 
         Intent intent = getIntent();
+
         Uri uri = intent.getParcelableExtra(NotesProvider.CONTENT_ITEM_TYPE);
+
         if (uri == null) {
             action = Intent.ACTION_INSERT;
             setTitle(getString(R.string.new_note));
+        } else {
+            action = Intent.ACTION_EDIT;
+            noteFilter = DBOpenHelper.NOTE_ID + "=" + uri.getLastPathSegment();
+
+            Cursor cursor = getContentResolver().query(uri,
+                    DBOpenHelper.ALL_COLUMNS, noteFilter, null, null);
+            cursor.moveToFirst();
+            oldText = cursor.getString(cursor.getColumnIndex(DBOpenHelper.NOTE_TEXT));
+            editor.setText(oldText);
+            editor.requestFocus();
         }
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -68,8 +84,25 @@ public class EditorActivity extends AppCompatActivity {
                 } else {
                     InsertNote(newText);
                 }
+                break;
+            case Intent.ACTION_EDIT:
+                if (newText.length() == 0) {
+//                    deleteNote();
+                } else if (oldText.equals(newText)) {
+                    setResult(RESULT_CANCELED);
+                } else {
+                    updateNote(newText);
+                }
         }
         finish();
+    }
+
+    private void updateNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, noteText);
+        getContentResolver().update(NotesProvider.CONTENT_URI, values, noteFilter, null);
+        Toast.makeText(this, getString(R.string.note_updated), Toast.LENGTH_SHORT).show();
+        setResult(RESULT_OK);
     }
 
     private void InsertNote(String noteText) {
--- app/src/main/java/com/ms_wit/mtmnotesplus/MainActivity.java
+++ app/src/main/java/com/ms_wit/mtmnotesplus/MainActivity.java
@@ -18,6 +18,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -28,8 +29,7 @@ import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity
-implements LoaderManager.LoaderCallbacks<Cursor>
-{
+        implements LoaderManager.LoaderCallbacks<Cursor> {
     private static final int EDITOR_REQUEST_CODE = 1001;
     private CursorAdapter cursorAdapter;
     /**
@@ -48,11 +48,21 @@ implements LoaderManager.LoaderCallbacks<Cursor>
         ListView list = (ListView) findViewById(android.R.id.list);
         list.setAdapter(cursorAdapter);
 
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                Uri uri = Uri.parse(NotesProvider.CONTENT_URI + "/" + id);
+                intent.putExtra(NotesProvider.CONTENT_ITEM_TYPE, uri);
+                startActivityForResult(intent, EDITOR_REQUEST_CODE);
+            }
+        });
+
+
         getLoaderManager().initLoader(0, null, this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
@@ -170,8 +180,8 @@ implements LoaderManager.LoaderCallbacks<Cursor>
         startActivityForResult(intent, EDITOR_REQUEST_CODE);
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data ) {
-        if (requestCode ==EDITOR_REQUEST_CODE && resultCode == RESULT_OK) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == EDITOR_REQUEST_CODE && resultCode == RESULT_OK) {
             restartLoader();
         }
     }
