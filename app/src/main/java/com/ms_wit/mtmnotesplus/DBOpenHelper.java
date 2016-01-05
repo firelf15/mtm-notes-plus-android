26b21582caffd2dda1d86709f5fa1818f4625e6c
Monika Spielman <github@firelf.com>
2016-01-04 18:22:07 -0800

    MTP-97 WIP maybe I've defined the SQL

--- app/src/main/java/com/ms_wit/mtmnotesplus/DBOpenHelper.java
+++ app/src/main/java/com/ms_wit/mtmnotesplus/DBOpenHelper.java
@@ -9,31 +9,40 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DBOpenHelper extends SQLiteOpenHelper {
 
-    //This Gist is for use in the lynda.com course
-//Create a Note-Taking App for Android
-// #PlainOlNotes2
-//*****************************************
-
     //Constants for db name and version
     private static final String DATABASE_NAME = "notes.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
-    //Constants for identifying table and columns
-    public static final String TABLE_NOTES = "notes";
+    // Table Names
+    private static final String TABLE_CONTENTS = "contents";
+    private static final String TABLE_NOTES = "notes";
+
+    // CONTENTS Table - column names
+    private static final String CONTENT_ID = "content_id";
+    private static final String CONTENT_TITLE = "content_title";
+    private static final String CONTENT_PICTURE = "content_picture";
+    private static final String CONTENT_PARAGRAPH = "content_paragraph";
+
+    // NOTES Table - column names
     public static final String NOTE_ID = "_id";
     public static final String NOTE_TEXT = "noteText";
     public static final String NOTE_CREATED = "noteCreated";
+    private static final String RELATED_CONTENT_ID = "related_content_id";
 
-    public static final String[] ALL_COLUMNS =
-            {NOTE_ID, NOTE_TEXT, NOTE_CREATED};
+    // CONTENTS Table Create Statements
+    private static final String CREATE_TABLE_CONTENTS =
+            "CREATE TABLE " + TABLE_CONTENTS + "(" +
+                    CONTENT_ID + " INTEGER PRIMARY KEY," +
+                    CONTENT_TITLE + " TEXT," +
+                    CONTENT_PICTURE + " PICTURE BLOB" +
+                    CONTENT_PARAGRAPH + " TEXT" + ")";
 
-    //SQL to create table
-    private static final String TABLE_CREATE =
+    // NOTES Table Create Statement
+    private static final String CREATE_TABLE_NOTES =
             "CREATE TABLE " + TABLE_NOTES + " (" +
                     NOTE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     NOTE_TEXT + " TEXT, " +
-                    NOTE_CREATED + " TEXT default CURRENT_TIMESTAMP" +
-                    ")";
+                    RELATED_CONTENT_ID + " INTEGER, " + NOTE_CREATED + " TEXT default CURRENT_TIMESTAMP" + ")";
 
     public DBOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -41,11 +50,13 @@ public class DBOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(TABLE_CREATE);
+        db.execSQL(CREATE_TABLE_CONTENTS);
+        db.execSQL(CREATE_TABLE_NOTES);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTENTS);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTES);
         onCreate(db);
     }
